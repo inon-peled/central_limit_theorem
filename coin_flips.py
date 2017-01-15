50399844f4bc74cde401c35a57e7e804f25175aa
inonpeled <inon.peled@gmail.com>
2017-01-15 19:17:10 +0200

    Change delimit() to be purely functional

--- coin_flips.py
+++ coin_flips.py
@@ -16,12 +16,10 @@ def distribution_num_heads(num_samples, num_flips_per_sample):
 
 
 def delimit(sequence_of_ones_and_zeros):
-    last_element = None
-    for index, element in enumerate(sequence_of_ones_and_zeros):
-        if last_element is not None and element != last_element:
-            yield index - 1
-        last_element = element
-    yield index
+    t = itertools.tee(sequence_of_ones_and_zeros)
+    z = zip(t[0], itertools.islice(t[1], 1, None))
+    return map(lambda pair: pair[0],
+               filter(lambda pair: pair[1][0] != pair[1][1], enumerate(z)))
 
 
 def distribution_average_repeated_result_length(num_samples, num_flips_per_sample):
@@ -42,7 +40,7 @@ def distribution_average_repeated_result_length(num_samples, num_flips_per_sampl
 
 
 def experiment_and_plot(distribution_function, num_samples, num_flips_per_sample):
-    dist = distribution_function(num_samples, num_flips_per_sample)
+    dist = list(distribution_function(num_samples, num_flips_per_sample))
     open('dist.csv', 'w').write('\n'.join(map(str, enumerate(dist))) + '\n')
     dist_non_zero = list(filter(lambda i_v: i_v[1] != 0, enumerate(dist)))
     min_x, max_x = dist_non_zero[0][0], dist_non_zero[-1][0]
@@ -53,4 +51,4 @@ def experiment_and_plot(distribution_function, num_samples, num_flips_per_sample
 
 if __name__ == '__main__':
     # print(list(delimit('11100110000')))
-    experiment_and_plot(distribution_average_repeated_result_length, 1000000, 100)
+    experiment_and_plot(distribution_average_repeated_result_length, 100000, 100)
